ca30059a71cc8945fa51f216c67344d05deeb15f
cwtshh <gucosta1719@gmail.com>
2023-08-18 16:55:00 -0300

    update user function

--- middlewares/userValidations.js
+++ middlewares/userValidations.js
@@ -42,7 +42,21 @@ const loginValidation = () => {
     ]
 };
 
+const userUpdateValidation = () => {
+    return [
+        body('name')
+            .optional()
+            .isLength({ min: 3})
+            .withMessage('O nome deve ter no minimo 3 caracteres.'),
+        body('password')
+            .optional()
+            .isLength({ min: 5})
+            .withMessage('A senha deve ter no minimo 5 caracteres.'),
+    ]
+}
+
 module.exports = {
     userCreateValidation,
     loginValidation,
+    userUpdateValidation,
 };
